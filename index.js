0cae1a7756b4ec6763e8cd3f316335f8d69ba025
unknown <carloshenrique@lekto.com.br>
2023-08-16 16:46:05 -0300

    fix(node): add body-parser

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
 
 app.set('view engine', 'ejs');
 var path = require('path')
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
 
 app.get("/", (req, res) => {
     res.render("index")
 })
 
-app.get("/perguntar", (req, res) => {
-    res.render("perguntar")
+app.get("/ask", (req, res) => {
+    res.render("ask")
+})
+
+app.post("/save", (req, res) => {
+   var title = req.body.title
+   var desc = req.body.desc
+
+    res.send("Titulo" + title + " " + "Desc" + " " + desc)
 })
 
 app.listen(1206), () => {
